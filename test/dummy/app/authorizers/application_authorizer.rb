0109b33f10ac5adf38d352fc1b724384becd92f8
juniormesquitadandao <juniormesquitadandao@gmail.com>
2016-04-26 04:48:14 -0300

    rename

--- test/dummy/app/authorizers/application_authorizer.rb
+++ test/dummy/app/authorizers/application_authorizer.rb
@@ -1,4 +1,23 @@
 class ApplicationAuthorizer < ApplicationAuthorizeRequest
+  # All methods below can be used in all authorizers.
+
+  # Skip the following parameters for all requests [:utf8, :_method, :authenticity_token, :commit].
   skip_default_params
+
+  # Authorize the predefined actions with their respective parameters.
   authorize_default_params
+
+  # Specify which parameters you want to skip all requests.
+  # def skiped_params
+  #   [:param_one, :param_two, ...]
+  # end
+
+  # Specify which parameters you want to use for each action.
+  # def authorize_params
+  #   {
+  #     index: {
+  #       param_one: [], param_two: %w( value_one, value_two, ...)
+  #     }
+  #   }
+  # end
 end
