3ab3c6fcb047cbe45abe33a613fd303c9a5ac515
juniormesquitadandao <juniormesquitadandao@gmail.com>
2016-04-26 05:14:10 -0300

    device

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     # You can have the root of your site routed with "root"
     root 'application#index'
 
+    get 'users/index'
+
     # Example of regular route:
     #   get 'products/:id' => 'catalog#view'
 
