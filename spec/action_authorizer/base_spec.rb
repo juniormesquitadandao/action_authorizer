6234df64f97ddf3f8b20a8df2935f6717eea3ea1
juniormesquitadandao <juniormesquitadandao@gmail.com>
2016-04-29 16:01:38 -0300

    action_authorizer

--- spec/action_authorizer/base_spec.rb
+++ spec/action_authorizer/base_spec.rb
@@ -1,6 +1,9 @@
 RSpec.describe 'Base' do
 
-  let(:authenticated) { double('Authenticated') }
+  let(:guest) { nil }
+  let(:user_one) { double('Authenticated', house_ids: [1]) }
+  let(:user_two) { double('Authenticated', house_ids: [2]) }
+  let(:admin) { double('Authenticated', house_ids: [1, 2]) }
 
   def request controller, action, others = {}
      params = {
@@ -15,160 +18,123 @@ RSpec.describe 'Base' do
 
   context '#index' do
     describe 'not authorize' do
-      it { expect(HousesAuthorizer.new(authenticated, 'index')).not_to be_authorized }
+      it { expect(HousesAuthorizer.new(guest, 'index')).not_to be_authorized }
     end
 
     describe 'authorize' do
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:index){ true }
-        expect(HousesAuthorizer.new(authenticated, 'index')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_one, 'index')).to be_authorized }
+
+      it { expect(HousesAuthorizer.new(user_two, 'index')).to be_authorized }
+
+      it { expect(HousesAuthorizer.new(admin, 'index')).to be_authorized }
     end
   end
 
   context '#show' do
     describe 'not authorize' do
-      it { expect(HousesAuthorizer.new(authenticated, 'show')).not_to be_authorized }
+      it { expect(HousesAuthorizer.new(guest, 'show', id: '1')).not_to be_authorized }
 
-      it { expect(HousesAuthorizer.new(authenticated, 'show', id: '1')).not_to be_authorized }
+      it { expect(HousesAuthorizer.new(user_one, 'show', id: '2')).not_to be_authorized }
 
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:show){ {id: []} }
-        expect(HousesAuthorizer.new(authenticated, 'show', id: '1')).not_to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_two, 'show', id: '1')).not_to be_authorized }
     end
 
     describe 'authorize' do
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:show){ true }
-        expect(HousesAuthorizer.new(authenticated, 'show')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_one, 'show', id: '1')).to be_authorized }
 
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:show){ true }
-        expect(HousesAuthorizer.new(authenticated, 'show', id: '1')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_two, 'show', id: '2')).to be_authorized }
 
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:show){ {id: [1]} }
-        expect(HousesAuthorizer.new(authenticated, 'show', id: '1')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(admin, 'show', id: '1')).to be_authorized }
+
+      it { expect(HousesAuthorizer.new(admin, 'show', id: '2')).to be_authorized }
     end
   end
 
   context '#new' do
     describe 'not authorize' do
-      it { expect(HousesAuthorizer.new(authenticated, 'new')).not_to be_authorized }
+      it { expect(HousesAuthorizer.new(guest, 'new')).not_to be_authorized }
     end
 
     describe 'authorize' do
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:new){ true }
-        expect(HousesAuthorizer.new(authenticated, 'new')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_one, 'new')).to be_authorized }
+
+      it { expect(HousesAuthorizer.new(user_two, 'new')).to be_authorized }
+
+      it { expect(HousesAuthorizer.new(admin, 'new')).to be_authorized }
     end
   end
 
   context '#edit' do
     describe 'not authorize' do
-      it { expect(HousesAuthorizer.new(authenticated, 'edit')).not_to be_authorized }
+      it { expect(HousesAuthorizer.new(guest, 'edit', id: '1')).not_to be_authorized }
 
-      it { expect(HousesAuthorizer.new(authenticated, 'edit', id: '1')).not_to be_authorized }
+      it { expect(HousesAuthorizer.new(user_one, 'edit', id: '2')).not_to be_authorized }
 
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:edit){ {id: []} }
-        expect(HousesAuthorizer.new(authenticated, 'edit', id: '1')).not_to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_two, 'edit', id: '1')).not_to be_authorized }
     end
 
     describe 'authorize' do
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:edit){ true }
-        expect(HousesAuthorizer.new(authenticated, 'edit')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_one, 'edit', id: '1')).to be_authorized }
 
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:edit){ true }
-        expect(HousesAuthorizer.new(authenticated, 'edit', id: '1')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_two, 'edit', id: '2')).to be_authorized }
 
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:edit){ {id: [1]} }
-        expect(HousesAuthorizer.new(authenticated, 'edit', id: '1')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(admin, 'edit', id: '1')).to be_authorized }
+
+      it { expect(HousesAuthorizer.new(admin, 'edit', id: '2')).to be_authorized }
     end
   end
 
   context '#create' do
     describe 'not authorize' do
-      it { expect(HousesAuthorizer.new(authenticated, 'create')).not_to be_authorized }
+      it { expect(HousesAuthorizer.new(guest, 'create')).not_to be_authorized }
     end
 
     describe 'authorize' do
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:create){ true }
-        expect(HousesAuthorizer.new(authenticated, 'create')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_one, 'create')).to be_authorized }
+
+      it { expect(HousesAuthorizer.new(user_two, 'create')).to be_authorized }
+
+      it { expect(HousesAuthorizer.new(admin, 'create')).to be_authorized }
     end
   end
 
   context '#update' do
     describe 'not authorize' do
-      it { expect(HousesAuthorizer.new(authenticated, 'update')).not_to be_authorized }
+      it { expect(HousesAuthorizer.new(guest, 'update', id: '1')).not_to be_authorized }
 
-      it { expect(HousesAuthorizer.new(authenticated, 'update', id: '1')).not_to be_authorized }
+      it { expect(HousesAuthorizer.new(user_one, 'update', id: '2')).not_to be_authorized }
 
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:update){ {id: []} }
-        expect(HousesAuthorizer.new(authenticated, 'update', id: '1')).not_to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_two, 'update', id: '1')).not_to be_authorized }
     end
 
     describe 'authorize' do
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:update){ true }
-        expect(HousesAuthorizer.new(authenticated, 'update')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_one, 'update', id: '1')).to be_authorized }
 
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:update){ true }
-        expect(HousesAuthorizer.new(authenticated, 'update', id: '1')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_two, 'update', id: '2')).to be_authorized }
 
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:update){ {id: [1]} }
-        expect(HousesAuthorizer.new(authenticated, 'update', id: '1')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(admin, 'update', id: '1')).to be_authorized }
+
+      it { expect(HousesAuthorizer.new(admin, 'update', id: '2')).to be_authorized }
     end
   end
 
   context '#destroy' do
     describe 'not authorize' do
-      it { expect(HousesAuthorizer.new(authenticated, 'destroy')).not_to be_authorized }
+      it { expect(HousesAuthorizer.new(guest, 'destroy', id: '1')).not_to be_authorized }
 
-      it { expect(HousesAuthorizer.new(authenticated, 'destroy', id: '1')).not_to be_authorized }
+      it { expect(HousesAuthorizer.new(user_one, 'destroy', id: '2')).not_to be_authorized }
 
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:destroy){ {id: []} }
-        expect(HousesAuthorizer.new(authenticated, 'destroy', id: '1')).not_to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_two, 'destroy', id: '1')).not_to be_authorized }
     end
 
     describe 'authorize' do
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:destroy){ true }
-        expect(HousesAuthorizer.new(authenticated, 'destroy')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_one, 'destroy', id: '1')).to be_authorized }
 
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:destroy){ true }
-        expect(HousesAuthorizer.new(authenticated, 'destroy', id: '1')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(user_two, 'destroy', id: '2')).to be_authorized }
 
-      it do
-        expect_any_instance_of(HousesAuthorizer).to receive(:destroy){ {id: [1]} }
-        expect(HousesAuthorizer.new(authenticated, 'destroy', id: '1')).to be_authorized
-      end
+      it { expect(HousesAuthorizer.new(admin, 'destroy', id: '1')).to be_authorized }
+
+      it { expect(HousesAuthorizer.new(admin, 'destroy', id: '2')).to be_authorized }
     end
   end
 
--- spec/support/authorizers/houses_authorizer.rb
+++ spec/support/authorizers/houses_authorizer.rb
@@ -1,35 +1,31 @@
 class HousesAuthorizer < ApplicationAuthorizer
 
   def index
-    # true
+    @authenticated
   end
 
   def show
-    # true
-    # { id: authenticated.house_ids }
+    { id: @authenticated.try(:house_ids) || [] }
   end
 
   def new
-    # true
+    @authenticated
   end
 
   def edit
-    # true
-    # { id: authenticated.house_ids }
+    { id: @authenticated.try(:house_ids) || [] }
   end
 
   def create
-    # true
+    @authenticated
   end
 
   def update
-    # true
-    # { id: authenticated.house_ids }
+    { id: @authenticated.try(:house_ids) || [] }
   end
 
   def destroy
-    # true
-    # { id: authenticated.house_ids }
+    { id: @authenticated.try(:house_ids) || [] }
   end
 
 end
