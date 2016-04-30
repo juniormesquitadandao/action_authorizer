29fabde3bce3177196126aa37c8765a0835e7923
juniormesquitadandao <juniormesquitadandao@gmail.com>
2016-04-29 23:23:06 -0300

    action_authorizer

---
+++ spec/support/authorizers/country/citys_authorizer.rb
@@ -0,0 +1,47 @@
+class Country::CitysAuthorizer < ApplicationAuthorizer
+
+  def index
+    @authenticated
+  end
+
+  def show
+    if @authenticated.try(:user?)
+      { id: @authenticated.house_ids }
+    elsif @authenticated.try(:admin?)
+      true
+    end
+  end
+
+  def new
+    @authenticated
+  end
+
+  def edit
+    if @authenticated.try(:user?)
+      { id: @authenticated.house_ids }
+    elsif @authenticated.try(:admin?)
+      true
+    end
+  end
+
+  def create
+    @authenticated
+  end
+
+  def update
+    if @authenticated.try(:user?)
+      { id: @authenticated.house_ids }
+    elsif @authenticated.try(:admin?)
+      true
+    end
+  end
+
+  def destroy
+    if @authenticated.try(:user?)
+      { id: @authenticated.house_ids }
+    elsif @authenticated.try(:admin?)
+      true
+    end
+  end
+
+end
