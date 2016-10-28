58f327841e995888e4b6c3d5adb96404849ae6f8
juniormesquitadandao <juniormesquitadandao@gmail.com>
2016-10-28 19:50:27 -0300

    1.3

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :name, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ActionAuthorizer::Application.routes.draw do
+  devise_for :users
+  resources :products
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
