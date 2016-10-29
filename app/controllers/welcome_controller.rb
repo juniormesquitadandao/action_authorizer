bf336e24ea629b325ba3063e84922817b796fcdd
juniormesquitadandao <juniormesquitadandao@gmail.com>
2016-10-28 22:05:38 -0300

    1.3

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
   respond_to :html
 
   def index
-    @products = Product.for current_user
+    @products = Product.all
     respond_with(@products)
   end
 
