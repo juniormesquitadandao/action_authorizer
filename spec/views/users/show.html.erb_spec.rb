c0603f9bcd490616d93d50fe09a72dff517aa81c
juniormesquitadandao <juniormesquitadandao@gmail.com>
2016-10-30 16:29:52 -0300

    1.3

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,16 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe "users/show", type: :view do
+RSpec.describe "users/show", :type => :view do
   before(:each) do
-    @user = assign(:user, User.create!(
-      :email => "Email",
-      :admin => false
-    ))
+    @link_to_edit = link_to 'Edit', edit_user_path(@user)
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Email/)
-    expect(rendered).to match(/false/)
+
+    expect(rendered).to match(/User/)
+    expect(rendered).to match(/No/)
+    expect(rendered).to match(/user@email.com/)
+  end
+
+  it "when admin?" do
+    sign_in @admin
+
+    render
+
+    expect(rendered).to match(@link_to_edit)
+  end
+
+  it "when user" do
+    sign_in @user
+
+    render
+
+    expect(rendered).not_to match(@link_to_edit)
+  end
+
+  it "when other user" do
+    sign_in @other
+
+    render
+
+    expect(rendered).not_to match(@link_to_edit)
   end
 end
