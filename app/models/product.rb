f04870c6c66b34a28d3257c4992880141cd1c8ba
juniormesquitadandao <juniormesquitadandao@gmail.com>
2016-10-30 15:09:15 -0300

    1.3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,21 @@
 class Product < ActiveRecord::Base
   belongs_to :user
+
+  validates_presence_of :name, :user_id
+
+  validate :user_id_reserved
+
+  def self.for user
+    if user && !user.admin?
+      user.products.to_a
+    else
+      all
+    end
+  end
+
+  private
+
+  def user_id_reserved
+    errors.add(:user_id, :exclusion) if user.try(:reload).try(:admin?)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ActiveRecord::Base
+  has_many :products, dependent: :restrict_with_error
+
+  validates_presence_of :name
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def to_s
+    "#{name} (#{email})"
+  end
+
+  def self.for user
+    if user && user.admin?
+      where("id <> #{user.id}").to_a
+    else
+      []
+    end
+  end
 end
