bc86c97fe1d03ee6a3a56f7924a021e1c70c9523
juniormesquitadandao <juniormesquitadandao@gmail.com>
2016-11-20 21:47:33 -0300

    1.3.1: refactory concerns

--- lib/action_authorizer/helper.rb
+++ lib/action_authorizer/helper.rb
@@ -3,21 +3,17 @@ require 'active_support/concern'
 module ActionAuthorizer::Helper
   extend ActiveSupport::Concern
 
-  included do
+  def authenticated
+    current_user
+  end
 
-    def authenticated
-      current_user
-    end
+  def authorized? controller, action, params = {}
+    !unauthorized? controller, action, params
+  end
 
-    def authorized? controller, action, params = {}
-      !unauthorized? controller, action, params
-    end
-
-    def unauthorized? controller, action, params = {}
-      authorizer = "#{controller.to_s}_authorizer".classify.constantize
-      authorizer.new(authenticated, action.to_sym, params).unauthorized?
-    end
-
+  def unauthorized? controller, action, params = {}
+    authorizer = "#{controller.to_s}_authorizer".classify.constantize
+    authorizer.new(authenticated, action.to_sym, params).unauthorized?
   end
 
 end
