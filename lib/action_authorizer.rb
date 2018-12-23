bfb7bbf16b31a2b8aed2846e86c4656f0b88d505
juniormesquitadandao <juniormesquitadandao@gmail.com>
2018-12-23 07:00:32 -0300

    v2 demo

---
+++ lib/action_authorizer.rb
@@ -0,0 +1,2 @@
+module ActionAuthorizer
+end
--- lib/generators/action_authorizer/install/install_generator.rb
+++ lib/generators/action_authorizer/install/install_generator.rb
@@ -7,24 +7,4 @@ class ActionAuthorizer::InstallGenerator < Rails::Generators::Base # :nodoc:
       end
     RUBY
   end
-
-  # def create_action_authorizer_helper_file
-  #   create_file 'app/helpers/action_authorizer_helper.rb', <<~RUBY
-  #     module ActionAuthorizerHelper
-  #       # Add helpers to check authorization authenticated.
-  #       # def unauthorized? controller, action, params = {}
-  #       # def authorized? controller, action, params = {}
-  #       # ex.:
-  #       #   <%= link_to 'Models', models_path if authorized? :models, :index %>
-  #       #   <%= link_to 'Models Dashboard', dashboard_models_path if authorized? 'dashborad/models', :index %>
-  #       #   <%= link_to 'Model', model_path(@model) if authorized? :models, :show, id: @model.id %>
-  #       #   <%= link_to 'Model', edit_model_path(@model) if authorized? :models, :edit, id: @model.to_param %>
-  #       include ActionAuthorizer::Helper
-
-  #       # def authenticated
-  #       #   current_user
-  #       # end
-  #     end
-  #   RUBY
-  # end
 end
