68c89c3e9d4f08a2083bb3391ec2d36c1e08746b
juniormesquitadandao <juniormesquitadandao@gmail.com>
2018-12-23 06:41:00 -0300

    v2 demo

--- lib/generators/action_authorizer/install/install_generator.rb
+++ lib/generators/action_authorizer/install/install_generator.rb
@@ -2,30 +2,29 @@ class ActionAuthorizer::InstallGenerator < Rails::Generators::Base # :nodoc:
   desc 'Creates an initializer with default action_authorizer configuration and copy locale file'
 
   def create_application_authorize_file
-    create_file 'app/authorizers/application_authorizer.rb', <<-RUBY
-class ApplicationAuthorizer < ActionAuthorizer::Base
-end
+    create_file 'app/authorizers/application_authorizer.rb', <<~RUBY
+      class ApplicationAuthorizer < ActionAuthorizer::Base
+      end
     RUBY
   end
 
-  def create_action_authorizer_helper_file
-    create_file 'app/helpers/action_authorizer_helper.rb', <<-RUBY
-module ActionAuthorizerHelper
-  # Add helpers to check authorization authenticated.
-  # def unauthorized? controller, action, params = {}
-  # def authorized? controller, action, params = {}
-  # ex.:
-  #   <%= link_to 'Models', models_path if authorized? :models, :index %>
-  #   <%= link_to 'Models Dashboard', dashboard_models_path if authorized? 'dashborad/models', :index %>
-  #   <%= link_to 'Model', model_path(@model) if authorized? :models, :show, id: @model.id %>
-  #   <%= link_to 'Model', edit_model_path(@model) if authorized? :models, :edit, id: @model.to_param %>
-  include ActionAuthorizer::Helper
+  # def create_action_authorizer_helper_file
+  #   create_file 'app/helpers/action_authorizer_helper.rb', <<~RUBY
+  #     module ActionAuthorizerHelper
+  #       # Add helpers to check authorization authenticated.
+  #       # def unauthorized? controller, action, params = {}
+  #       # def authorized? controller, action, params = {}
+  #       # ex.:
+  #       #   <%= link_to 'Models', models_path if authorized? :models, :index %>
+  #       #   <%= link_to 'Models Dashboard', dashboard_models_path if authorized? 'dashborad/models', :index %>
+  #       #   <%= link_to 'Model', model_path(@model) if authorized? :models, :show, id: @model.id %>
+  #       #   <%= link_to 'Model', edit_model_path(@model) if authorized? :models, :edit, id: @model.to_param %>
+  #       include ActionAuthorizer::Helper
 
-  # def authenticated
-  #   current_user
+  #       # def authenticated
+  #       #   current_user
+  #       # end
+  #     end
+  #   RUBY
   # end
-end
-    RUBY
-  end
-
 end
